7a78f81c34d7c30b4e6001dfa5893259df128958
DariusBlac <legionnsspace@gmail.com>
2023-04-23 13:42:11 +0200

    fix hero and portfolio set

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,18 +1,12 @@
-(() => {
+
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+const closeMenuBtn = document.querySelector('.js-close-menu');
+const body = document.querySelector('body');
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    body.classList.toggle('m-ryzhak');
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
@@ -22,7 +16,5 @@
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+    body.classList.remove('m-ryzhak');
   });
-})();
